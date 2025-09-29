0e8b0c4c0287ff24e1c5bcb5404ca0f1fb5a7991
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 13:37:58 -0400

    * worked on in class together * comments for class solution in first if statement * the way I would code it remains in the following if-else statements

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,11 +1,11 @@
 package com.pluralsight;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class FullNameParser {
     public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
+    int i = 1;
 
     System.out.print("Please enter your full name: ");
     String fullName = keyboard.nextLine().trim();
@@ -13,9 +13,24 @@ public class FullNameParser {
     // need to use the array index numbers to split strings into different variables to print
         // also need to figure out how to print first and last name when no middle name is given
     String[] parsedNames = fullName.split("\\s+");
-        System.out.println(parsedNames[0]);
+    // fullName.split("\\ "); also works
 
+    if (parsedNames.length < 3) {
+        String firstName = parsedNames[0];
+        String lastName = parsedNames[1];
+        System.out.printf("First Name: %s\nLast Name: %s", firstName, lastName);
+        // easier method to print the same result above^
+        // souf(First Name: %s\nLast Name: %s, parsedNames[0], parsedNames[1])
+        }
+    else if (parsedNames.length < 4){
+        String firstName = parsedNames[0];
+        String middleName = parsedNames[1];
+        String lastName = parsedNames[2];
+        System.out.printf("First Name: %s\nMiddle Name: %s\nLast Name: %s", firstName, middleName, lastName);
+        }
+    else {
+        System.out.println("Invalid Name Format");
+    } // end of else statement
 
-
-    }
+    } // end of main()
 }
