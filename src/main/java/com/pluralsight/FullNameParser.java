ed1e6375cdfa1033d32ab76e39517822dcf1e63e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-29 11:45:40 -0400

    * initialized Scanner and fullName variable * added sout to ask user for name

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class FullNameParser {
     public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Please enter your full name: ");
+    String fullName = keyboard.nextLine().trim();
+
+
 
     }
 }
